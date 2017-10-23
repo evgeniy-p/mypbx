eb947d0c335387a60b6b9d099aae19a397c351dc
Evgeniy-p <mariwield@gmail.com>
2017-10-23 21:09:46 +0300

    update

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import menu
 import sys
 
 
-def set_ru_lang():
-    login_form.LANG = 1
-    menu_form.LANG = 1
+def set_lang(pref):
+    login_form.LANG = pref
+    menu_form.LANG = pref
     login_form.retranslateUi(login_form.start_window)
     menu_form.retranslateUi(menu_form.menu_window)
+
+
+def set_ru_lang():
+    set_lang(1)
 
 
 def set_en_lang():
-    login_form.LANG = 0
-    menu_form.LANG = 0
-    login_form.retranslateUi(login_form.start_window)
-    menu_form.retranslateUi(menu_form.menu_window)
+    set_lang(0)
 
 
 app = QApplication(sys.argv)
-
 """
 Инициализация формы логина
 """
@@ -39,7 +39,6 @@ login_form.pushButton_4.clicked.connect(set_en_lang)
 """
 Нажатие кнопок в меню 
 """
-
 menu_form.pushButton.clicked.connect(set_en_lang)
 menu_form.pushButton_2.clicked.connect(set_ru_lang)
 menu_form.pushButton_3.clicked.connect(menu_form.extension_but_clicked)
@@ -47,7 +46,6 @@ menu_form.pushButton_4.clicked.connect(menu_form.routing_but_clicked)
 menu_form.pushButton_5.clicked.connect(menu_form.cdr_but_clicked)
 menu_form.pushButton_6.clicked.connect(menu_form.dialrul_but_clicked)
 menu_form.pushButton_7.clicked.connect(menu_form.fax_but_clicked)
-
 """
 Выход
 """
